4e6f0b6ca92393db09a2bacbd2111b80c5758225
thgcamposantos <thgcamposantos@gmail.com>
2024-06-14 19:59:00 -0300

    Terceiro exerc

--- js/exerc1.js
+++ js/exerc1.js
@@ -17,3 +17,17 @@ let verificar_numero = parseInt(prompt('Digite um número: '));
     }else{
         console.log('Numero Impar')
     };
+/*Condicional Aninhada:
+Escreva um programa que receba três números e determine qual é o maior. Exiba "O maior número é X", onde X é o maior número.*/
+let numero1 = parseInt(prompt('Digite um numero: '));
+let numero2 = parseInt(prompt('Digite um numero: '));
+let numero3 = parseInt(prompt('Digite um numero: '));
+    if((numero1 == numero2) || (numero2 == numero3) || (numero1 == numero3)){
+        console.log('Use tres distintos')
+    }else if((numero1 > numero2) && (numero1 > numero3)){
+        console.log("Numero 1 é maior");
+    }else if((numero1 > numero2) && (numero1 < numero3)){
+        console.log("Número 3 é o maior numero")
+    }else if((numero1 < numero2) && (numero2 > numero3)){
+        console.log("Número 2 é maior")
+    };
